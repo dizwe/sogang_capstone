f950bda3a8a9de3b8b2988511f9deeca790fcf88
dizwe <dizwe2716@gmail.com>
2020-03-30 16:20:37 +0900

    ADD proposal of Project

--- learn/tcp/tcp_test.py
+++ learn/tcp/tcp_test.py
@@ -1,7 +1,10 @@
 import requests
 import random
 import time
+
+IP = "34.235.96.225:8000"
 for i in range(10):
     value = random.randint(1,100)
-    requests.get("https://api.thingspeak.com/update?api_key=BSHDWLYOIEV6NDJI&field1="+str(value))
+    response = requests.get(f"http://{IP}?field1="+str(value))
+    console.log(response.text)
     time.sleep(20)
